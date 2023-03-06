37434b2c5af4ec9c9c4b8e47f85ace7eec8d2390
policy745 <paulafolabi745@gmail.com>
2023-03-06 01:45:30 +0100

    This commit makes a whole lot of changes to my api

---
+++ WebApi0/WebApi/Data/WebApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Model.Domain;
+
+namespace WebApi.Data
+{
+    public class WebApiDbContext : DbContext
+    {
+        public WebApiDbContext(DbContextOptions<WebApiDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+
+
+    }
+}
--- WebApi0/WebApi/Program.cs
+++ WebApi0/WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
 namespace WebApi
 {
     public class Program
@@ -12,6 +15,8 @@ namespace WebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<WebApiDbContext>(options => options.UseSqlServer(
+                builder.Configuration.GetConnectionString("WebApiConnection")));
 
             var app = builder.Build();
 
